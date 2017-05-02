c3c1253f5efd8d9cb9b1aab0832dc321740d8da4
fancn <fanyj@yikeer.com>
2017-05-02 11:51:14 +0800

    static file

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('hello world!')
+app.get('/locales/:lng/*.json', function (req, res, next) {
+  res.sendFile(path.join(__dirname, 'src/' + req.originalUrl));
+});
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'src/index.html'));
 });
 
 var server = app.listen(3001, function () {
